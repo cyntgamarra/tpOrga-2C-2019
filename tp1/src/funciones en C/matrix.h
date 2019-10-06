0c87523ed3e4e3b82337ee6c42dbc16d8c2a5316
seba <seba21log@gmail.com>
2019-10-05 21:10:59 -0300

    merge

--- tp1/src/funciones en C/matrix.h
+++ tp1/src/funciones en C/matrix.h
@@ -16,11 +16,15 @@ matrix_t* create_matrix(size_t rows, size_t cols);
 // Destructor de matrix_t
 void destroy_matrix(matrix_t* m);
 
+// Destructor de matrix_t (usa myfree)
+void destroy_my_matrix(matrix_t* m);
+
+
 // Imprime matrix_t sobre el file pointer fp en el formato solicitado
 // por el enunciado
 int print_matrix(FILE* fp, matrix_t* m);
 
 // Multiplica las matrices en m1 y m2
-matrix_t* matrix_multiply(matrix_t* m1, matrix_t* m2);
+//matrix_t* matrix_multiply(matrix_t* m1, matrix_t* m2);
 
 #endif
